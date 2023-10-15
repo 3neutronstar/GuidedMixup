fdfcb43bb10ef2021530c178495f83c96b89eafd
Minsoo Kang <59332148+3neutronstar@users.noreply.github.com>
2023-10-15 19:54:35 +0900

    Update pairing.pyx

--- pairing.pyx
+++ pairing.pyx
@@ -7,13 +7,13 @@ INF= np.inf
 np.import_array()
 
 def onecycle_cover(np.ndarray[float, ndim=2] distance_matrix):
-    cdef int max_idx = np.argmax(distance_matrix)
     cdef int row =0
     cdef int col =0
     cdef int first_row = 0
     cdef int idx = 0
     cdef np.ndarray[int, ndim=1] sorted_indices = np.zeros(distance_matrix.shape[0], dtype=np.int32)
     np.fill_diagonal(distance_matrix, -INF)
+    cdef int max_idx = np.argmax(distance_matrix)
     row, col = max_idx//distance_matrix.shape[0], max_idx % distance_matrix.shape[0]
     first_row = deepcopy(row)
     sorted_indices[row] = col
